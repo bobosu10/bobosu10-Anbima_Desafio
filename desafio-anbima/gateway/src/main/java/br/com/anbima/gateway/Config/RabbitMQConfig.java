e1a7a81620eefdf6045dbed6e7017bd544d5dd24
bobosu10 <lucasbarbosadasilva3016@gmail.com>
2025-10-21 20:15:41 -0300

    fix:bug no rabbitMq resolvido agora modulo A e B funcionam em conjunto

--- desafio-anbima/gateway/src/main/java/br/com/anbima/gateway/Config/RabbitMQConfig.java
+++ desafio-anbima/gateway/src/main/java/br/com/anbima/gateway/Config/RabbitMQConfig.java
@@ -3,14 +3,21 @@ package br.com.anbima.gateway.Config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
+import org.springframework.amqp.support.converter.MessageConverter;
 
 @Configuration
 public class RabbitMQConfig {
 
-    public static final String QUEU_PEDIDOS_RECEBIDOS = "pedidos.recebidos";
+    public static final String QUEUE_PEDIDOS_RECEBIDOS = "pedidos.recebidos";
 
     @Bean
     public Queue queuePedidosRecebidos(){
-        return new Queue(QUEU_PEDIDOS_RECEBIDOS, true);
+        return new Queue(QUEUE_PEDIDOS_RECEBIDOS, true);
+    }
+
+    @Bean
+    public MessageConverter jsonMessageConverter(){
+        return new Jackson2JsonMessageConverter();
     }
 }
