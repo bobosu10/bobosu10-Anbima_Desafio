39f7287d859dac71d52d2c3b6241dc7ecf421a18
bobosu10 <lucasbarbosadasilva3016@gmail.com>
2025-10-21 00:32:39 -0300

    feat:detalhes para finalizar o gateway

--- desafio-anbima/gateway/src/main/java/br/com/anbima/gateway/Entity/Pedido.java
+++ desafio-anbima/gateway/src/main/java/br/com/anbima/gateway/Entity/Pedido.java
@@ -1,15 +1,19 @@
 package br.com.anbima.gateway.Entity;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
+
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
 
 @Table(name = "pedidos")
 @Data
 @Entity
-@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class Pedido {
 
     @Id
