33926a27a6961d824c82ca198cad35d53b7872e2
bobosu10 <lucasbarbosadasilva3016@gmail.com>
2025-10-21 15:29:58 -0300

    feat:testes unitarios adicionados

---
+++ desafio-anbima/gateway/src/test/java/br/com/anbima/gateway/Service/PedidosServiceTest.java
@@ -0,0 +1,149 @@
+package br.com.anbima.gateway.Service;
+
+import br.com.anbima.gateway.Dto.PedidoResponseDTO;
+import br.com.anbima.gateway.Entity.Pedido;
+import br.com.anbima.gateway.Messaging.PedidoProducer;
+import br.com.anbima.gateway.Repository.PedidoRepository;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.jupiter.api.Test;
+
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.*;
+
+@ExtendWith(MockitoExtension.class)
+class PedidosServiceTest {
+
+    @Mock
+    private PedidoRepository repository;
+
+    @Mock
+    private PedidoProducer producer;
+
+    @InjectMocks
+    private PedidoService service;
+
+
+    //configura o mock do repositorio para simular save
+    @BeforeEach
+    void setUp(){
+        when(repository.save(any(Pedido.class))).thenAnswer(invocationOnMock -> {
+            Pedido pedidoSalvo = invocationOnMock.getArgument(0);
+
+            pedidoSalvo.setId(1L);
+            pedidoSalvo.setCriadoEm(LocalDateTime.now());
+
+            return pedidoSalvo;
+        });
+
+        // Configura o mock do producer para não fazer nada (retorna void)
+        doNothing().when(producer).enviarPedidoRecebido(anyLong());
+    }
+
+    @Test
+    void testLinhaComDesconto(){
+        //cenario HAMBURGUER + CARNE + SALADA, 1 unid
+        String linha = "HAMBURGUERCARNE     SALADA    01COCA    ";
+
+        PedidoResponseDTO response = service.processarLinha(linha);
+
+        //verificacao
+        assertEquals(0, new BigDecimal("18.0").compareTo(response.valor()), "O valor com desconto deve ser 18.0");
+        assertEquals("RECEBIDO", response.status());
+        assertEquals(1L, response.id());
+
+        //verfica os mocks
+        verify(repository, times(1)).save(any(Pedido.class));
+        verify(producer, times(1)).enviarPedidoRecebido(1L);
+    }
+
+    @Test
+    void testLinhaSemDesconto(){
+
+        //cenario PASTEL + FRANGO + BACON, 2 unid
+        String linha = "PASTEL    FRANGO    BACON     02SUCO    ";
+
+        PedidoResponseDTO response = service.processarLinha(linha);
+
+        //verificacao
+        assertEquals(0, new BigDecimal("30.0").compareTo(response.valor()), "O valor sem desconto deve ser 30.0");
+        assertEquals("RECEBIDO", response.status());
+    }
+
+    @Test
+    void testLinhaOutros(){
+
+        //cenario tipo lanche = outros e 3 unids
+       String linha = "PIZZA     CALABRESA QUEIJO    03GUARANA ";
+
+       PedidoResponseDTO response = service.processarLinha(linha);
+
+       //verificacao
+        assertEquals(0, new BigDecimal("36.0").compareTo(response.valor()), "O valor 'outros' deve ser 36.0");
+        assertEquals("RECEBIDO", response.status());
+    }
+
+    @Test
+    void testLinhaQuantidadeZero(){
+
+        //cenario quantidade 00
+        String linha = "HAMBURGUERCARNE     SALADA    00COCA    ";
+
+        Exception exception = assertThrows(IllegalArgumentException.class, () -> {
+            service.processarLinha(linha);
+        });
+
+        assertEquals("A quantidade deve ser um valor entre 01 e 99.", exception.getMessage());
+
+        verify(repository, never()).save(any());
+        verify(producer, never()).enviarPedidoRecebido(anyLong());
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
---
+++ desafio-anbima/gateway/src/test/java/br/com/anbima/gateway/Util/ConversorTest.java
@@ -0,0 +1,78 @@
+package br.com.anbima.gateway.Util;
+import br.com.anbima.gateway.Entity.Pedido;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+class ConversorTest {
+
+    @Test
+     void testConverterLinha(){
+
+        //test 1:HAMBURGUER + CARNE + SALADA, 1 unid
+        String linhaValida = "HAMBURGUERCARNE     SALADA    01COCA    ";
+
+        Pedido pedido = Conversor.converter(linhaValida);
+
+        assertEquals("HAMBURGUER", pedido.getTipoLanche());
+        assertEquals("CARNE", pedido.getProteina());
+        assertEquals("SALADA", pedido.getAcompanhamento());
+        assertEquals(1, pedido.getQuantidade());
+        assertEquals("COCA", pedido.getBebida());
+    }
+
+    @Test
+    void testConverterLinhaComPaddingDif(){
+
+        //test 2:PASTEL + FRANGO + BACON, 2 unid
+
+        String linhaValida = "PASTEL    FRANGO    BACON     02SUCO    ";
+
+        Pedido pedido = Conversor.converter(linhaValida);
+
+        assertEquals("PASTEL", pedido.getTipoLanche());
+        assertEquals("FRANGO", pedido.getProteina());
+        assertEquals("BACON", pedido.getAcompanhamento());
+        assertEquals(2, pedido.getQuantidade());
+        assertEquals("SUCO", pedido.getBebida());
+    }
+
+    @Test
+    void testConverterLinhaTamanhoErrado(){
+
+        String linhaCurta = "STRING CURTA";
+
+        //teste da excecao
+
+        Exception exception = assertThrows(IllegalArgumentException.class, () -> {
+            Conversor.converter(linhaCurta);
+        });
+
+        //teste da mensagem de excecao
+
+        assertTrue(exception.getMessage().contains("A linha deve ter exatamente 40 caracteres"));
+    }
+
+    @Test
+    void testConverterQuebraDeLinha(){
+
+        //string com \n
+        String linhaQuebrada = "HAMBURGUERCARNE     SALADA    01COCA    \n";
+
+        Pedido pedido = Conversor.converter(linhaQuebrada);
+
+        assertEquals(1, pedido.getQuantidade());
+        assertEquals("COCA", pedido.getBebida());
+    }
+
+    @Test
+    void testConverterQuantidadeNaoNumerica(){
+
+        String linhaComErro = "HAMBURGUERCARNE     SALADA    AACOCA    ";
+
+        Exception exception = assertThrows(IllegalArgumentException.class, () -> {
+            Conversor.converter(linhaComErro);
+        });
+
+        assertEquals("O campo 'quantidade' (posições 31-32) deve ser numérico.", exception.getMessage());
+    }
+}
