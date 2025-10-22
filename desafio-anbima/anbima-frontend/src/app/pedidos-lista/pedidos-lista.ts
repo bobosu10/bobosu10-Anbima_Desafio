1ebefd745875f31b1299204dd7ddb98d0532cbca
bobosu10 <lucasbarbosadasilva3016@gmail.com>
2025-10-22 11:41:18 -0300

    front base pronto

--- desafio-anbima/anbima-frontend/src/app/pedidos-lista/pedidos-lista.ts
+++ desafio-anbima/anbima-frontend/src/app/pedidos-lista/pedidos-lista.ts
@@ -1,11 +1,40 @@
-import { Component } from '@angular/core';
+import { Component , OnInit} from '@angular/core';
+import { PedidoService , PedidoResponse } from '../pedido';
+import { CommonModule } from '@angular/common';
+import { error } from 'console';
 
 @Component({
   selector: 'app-pedidos-lista',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule],
   templateUrl: './pedidos-lista.html',
-  styleUrl: './pedidos-lista.css'
+  styleUrls: ['./pedidos-lista.css']
 })
-export class PedidosLista {
 
+export class PedidosLista implements OnInit {
+    pedidos: PedidoResponse[] = [];
+    erro: string | null = null;
+    isLoading: boolean = false;
+
+    constructor(private pedidoService: PedidoService) {}
+
+    ngOnInit(): void {
+        this.carregarPedidos();
+    }
+
+    carregarPedidos() {
+        this.isLoading = true;
+        this.erro = null;
+        this.pedidoService.listarTodosPedidos().subscribe({
+            next: (data) => {
+                this.pedidos = data;
+                this.isLoading = false;
+            },
+            error: (error) => {
+                console.error('Erro ao carregar pedidos:', error);
+                this.erro = error.message || 'Erro desconhecido ao carregar pedidos.';
+                this.isLoading = false;
+            }
+        });
+    }
 }
