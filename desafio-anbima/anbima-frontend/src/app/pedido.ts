35507d8de9a371c14b35ca67c71c516c54bf486d
bobosu10 <lucasbarbosadasilva3016@gmail.com>
2025-10-22 00:02:46 -0300

    feat:rotas configuradas+layouy basico+ config do httpClient +implementado o servico para chamar o backend

--- desafio-anbima/anbima-frontend/src/app/pedido.ts
+++ desafio-anbima/anbima-frontend/src/app/pedido.ts
@@ -13,5 +13,29 @@ export interface PedidoResponse {
     valor: number;
     status: string;
     criadoEm: string;
-  
+}
+
+@Injectable({
+    providedIn: 'root'
+})
+export class PedidoService {
+
+    private apiUrlGateway = 'http://localhost:8080/pedidos'; //url do gateway A
+
+    private apiUrlProcessor = 'http://localhost:8081/pedidos'; // URL do Processor B
+
+    constructor(private http: HttpClient) { }
+
+    enviarPedidoPosicional(linhaPosicional: string): Observable<PedidoResponse> {
+        const headers = new HttpHeaders({ 'Content-Type': 'text/plain' });
+        return this.http.post<PedidoResponse>('${this.apiUrlGateway}/posicional', linhaPosicional, { headers: headers});
+    }
+
+    listarTodosPedidos(): Observable<PedidoResponse[]> {
+        return this.http.get<PedidoResponse[]>(this.apiUrlProcessor);
+    }
+
+    buscarPedidoPorId(id:number): Observable<PedidoResponse> {
+        return this.http.get<PedidoResponse>('${this.apiUrlProcessor}/${id}');
+    }
 }
